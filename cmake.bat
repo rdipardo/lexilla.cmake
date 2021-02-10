f6610061fd9765382eefd6d717b743e27997e9ff
Robert Di Pardo <59004801+rdipardo@users.noreply.github.com>
2021-02-10 02:05:25 -0500

    [WIP] Pass sanitizer flag to linker during clang build

--- cmake.bat
+++ cmake.bat
@@ -5,11 +5,11 @@ xcopy /SY cmake\* lexilla
 cd lexilla
 
 @rem MinGW build
-cmake -Bbuild-mingw -H. -G"MinGW Makefiles"
+cmake -Bbuild-mingw -H. -G"MinGW Makefiles" -DCMAKE_C_COMPILER=gcc.exe -DCMAKE_CXX_COMPILER=g++.exe
 cmake --build ./build-mingw -- TestLexers
 
 @rem MSVC build
 CALL "%BOOTSTRAP_MSVC%" &
-cmake -Bbuild-msvc -H. -G"NMake Makefiles" -DCMAKE_CXX_COMPILER=cl.exe -DLEXILLA_STATIC=1
+cmake -Bbuild-msvc -H. -G"NMake Makefiles" -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DLEXILLA_STATIC=1
 cmake --build ./build-msvc -- TestLexers
 ENDLOCAL
