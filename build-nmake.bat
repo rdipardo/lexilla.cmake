62d9c3dc8c3600824231f9afc2049f4b822b59f5
Robert Di Pardo <59004801+rdipardo@users.noreply.github.com>
2021-04-12 21:36:07 -0400

    Update submodule

--- build-nmake.bat
+++ build-nmake.bat
@@ -2,5 +2,7 @@
 @rem build and test with NMake
 xcopy /SY cmakelists\* lexilla
 cd lexilla
-cmake -Bbuild-msvc -H. -G"NMake Makefiles"
+@rem build statically to work around CMake directory resolution issues 
+@rem and the resulting 1 exit code
+cmake -Bbuild-msvc -H. -G"NMake Makefiles" -DLEXILLA_STATIC=1
 cmake --build ./build-msvc -- TestLexers
