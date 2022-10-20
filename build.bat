403fe00f1b4abce37021e1dcaf3682debed562c3
Robert Di Pardo <Rob@AcerNotebook.local>
2022-10-19 21:20:43 -0400

    Remove pre-launch task from Windows configuration

--- build.bat
+++ build.bat
@@ -1,24 +1,27 @@
 @echo off
 setlocal
 SET "PLATFORM=x86"
-if not "%1"==[] (SET "PLATFORM=%1")
+if "%1" NEQ "" (SET "PLATFORM=%1")
 if not exist scintilla (hg clone http://hg.code.sf.net/p/scintilla/code scintilla)
-cd scintilla
+pushd scintilla
 hg pull
 hg up -C
-cd ..
+popd
 
 if not exist scite (hg clone http://hg.code.sf.net/p/scintilla/scite)
 
-cd scite\win32
+pushd scite\win32
 @rem  Make sure to trim any trailing backslash, e.g.:
 @rem  echo %WindowsSDKVersion%
 @rem  => 10.0.18362.0\
 msbuild /nologo /v:m /p:Platform=%PLATFORM%;WindowsTargetPlatformVersion=%WindowsSDKVersion:~0,-1%
+popd
 
-cd ..\..
-if ("%VSCODE_BUILD%"==[] AND NOT "%PLATFORM%"==[]) (
+if "%VSCODE_BUILD%"=="" goto :Launch
+
+exit /b 0
+
+:Launch
 if "%PLATFORM%"=="x86" (SET PLATFORM=)
 scite\win32\%PLATFORM%\Debug\SciTE.exe "%2"
-)
 endlocal
