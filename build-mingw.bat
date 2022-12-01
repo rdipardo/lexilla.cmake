75a9f474212ed49b4e742b5fd6a93e5eb8712e02
Robert Di Pardo <rob@bunsen.home>
2022-11-30 19:36:11 -0500

    Let build scripts accept parameters
    (cherry picked from commit cafbdabaa075b84174d3f2bbeb02e5979fdb5956)

--- build-mingw.bat
+++ build-mingw.bat
@@ -1,6 +1,9 @@
 @echo off
-@rem build and test with MinGW-w64
+SETLOCAL
+SET TASK=TestLexers
+IF NOT "%1"=="" (SET TASK=%1)
 xcopy /SY cmakelists\* lexilla
 cd lexilla
 cmake -Bbuild-mingw -H. -G"MinGW Makefiles" -DDEBUG=1
-cmake --build ./build-mingw -- TestLexers
+cmake --build ./build-mingw -- %TASK%
+ENDLOCAL
--- build-nmake.bat
+++ build-nmake.bat
@@ -1,8 +1,11 @@
 @echo off
-@rem build and test with NMake
+SETLOCAL
+SET TASK=TestLexers
+IF NOT "%1"=="" (SET TASK=%1)
 xcopy /SY cmakelists\* lexilla
 @rem export undecorated functions when DLL target is 32-bit
 echo D | xcopy /SY win32\* lexilla\win32
 cd lexilla
 cmake -Bbuild-msvc -H. -G"NMake Makefiles" -DDEBUG=1
-cmake --build ./build-msvc -- TestLexers
+cmake --build ./build-msvc -- %TASK%
+ENDLOCAL
