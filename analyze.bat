9a3021cd0495d78284696478fa77f048aee57dd2
Robert Di Pardo <rob@bunsen.home>
2023-03-08 20:30:09 -0500

    Restore C4365 diagnostic

--- analyze.bat
+++ analyze.bat
@@ -1,6 +1,10 @@
 @echo off
 SETLOCAL
+@rem Prefer the standalone command line tools, if available
+SET "BOOTSTRAP_MSVC=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat"
+IF EXIST "C:\Program Files\Microsoft Visual Studio\2022\" (
 SET "BOOTSTRAP_MSVC=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+)
 SET "FILE_NAME=*.cxx"
 SET "FILTER=cxx"
 SET LINTER_ARG=
@@ -24,7 +28,7 @@ echo.
 cd lexilla
 @echo on
 cl /analyze /I..\scintilla\include /Ilexlib /Iinclude /std:c++17 /nologo /Zi /TP /MP ^
-  /Wall /wd4820 /wd4365 /wd4514 /wd4625 /wd4626 /wd4710 /wd4996 /wd5204 /wd5027 /EHsc /O1 /MT /DNDEBUG /GL ^
+  /Wall /wd4820 /wd4514 /wd4625 /wd4626 /wd4710 /wd4996 /wd5204 /wd5027 /EHsc /O1 /MT /DNDEBUG /GL ^
   /c "lexers\%FILE_NAME%" | findstr /C:"%FILTER%"
 @echo off
 cd ..
--- build-mingw.bat
+++ build-mingw.bat
@@ -4,6 +4,6 @@ SET TASK=TestLexers
 IF NOT "%1"=="" (SET TASK=%1)
 xcopy /SY cmakelists\* lexilla
 cd lexilla
-cmake -Bbuild-mingw -H. -G"MinGW Makefiles" -DDEBUG=1
+cmake -Bbuild-mingw -H. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -G"MinGW Makefiles" -DDEBUG=1
 cmake --build ./build-mingw -- %TASK%
 ENDLOCAL
