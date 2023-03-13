796aa04bbd4ebad60b0b4dcded6007e0fd32c946
Robert Di Pardo <rob@bunsen.home>
2023-03-13 18:38:52 -0400

    Add Clang-Tidy check to lint scripts
    (cherry picked from commit ebced0813e8d3d01b3806955c1d6e439d9a57427)

--- lint.bat
+++ lint.bat
@@ -13,15 +13,16 @@ echo "Running cppcheck . . . ."
 @echo on
 cppcheck --enable=warning,performance,style,portability --suppressions-list=lexilla/cppcheck.suppress --max-configs=100 ^
 	-I lexilla/include -I lexilla/access -I lexilla/lexlib -I scintilla/include "-DSTDMETHODIMP_(type) type STDMETHODCALLTYPE" ^
-	--template=gcc --quiet "lexilla\lexers\%FILE_NAME%" | findstr /C:"%FILTER%"
+	--template=gcc --quiet --platform=native "lexilla\lexers\%FILE_NAME%" | findstr /C:"%FILTER%"
 @echo off
 echo.
 
 where clang 2>NUL: 1>NUL:
 IF %ERRORLEVEL%==0 (
-  @echo "Linting with Clang . . . ."
+  @echo "Linting with Clang-Tidy . . . ."
   @echo.
-  clang -I lexilla/include -I lexilla/access -I lexilla/lexlib -I scintilla/include -Wall -Wextra -pedantic --std=c++17 ^
-    -fsyntax-only -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE=1 "lexilla/lexers/%FILE_NAME%"
+  clang-tidy "lexilla/lexers/%FILE_NAME%" --config="" -- ^
+    -I lexilla/include -I lexilla/access -I lexilla/lexlib -I scintilla/include -Wall -Wextra -pedantic --std=c++17 ^
+    -fsyntax-only -DNDEBUG -D_CRT_SECURE_NO_DEPRECATE=1
 )
 ENDLOCAL
