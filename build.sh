9834bd2a37e3897ca95d33467062c888f05b22cd
Robert Di Pardo <rob@bunsen.localhost>
2024-12-14 18:22:58 -0500

    Suppress non-zero exit code on build script reruns

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 cp -vr cmakelists/* lexilla
 cp .gitignore lexilla
 cmake -B.cmake -H. -DDEBUG=1
-mv scite/scite/* scite/
+mv scite/scite/* scite/ || true
 rm -rf scite/lexilla scite/scintilla
 cmake --build .cmake -- scintilla
 pushd scite/gtk
